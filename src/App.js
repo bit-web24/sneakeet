3f17e80e043a30dd5efa5d3b92819d66d3c76819
bit-web24 <bitweb24@gmail.com>
2023-06-17 10:05:20 +0530

    config routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,70 @@
-import "./App.css";
-import Home from "./components/Home";
-import Products from "./components/Products";
-import Product from "./components/Products/Product";
-import Cart from "./components/Cart";
-import Favorites from "./components/Favorites";
-import Account from "./components/Account";
-import NotFound from "./components/NotFound";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import axios from 'axios';
+import jwtDecode from 'jwt-decode';
+
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Products from './components/Products';
+import Product from './components/Products/Product';
+import Cart from './components/Cart';
+import Favorites from './components/Favorites';
+import Account from './components/Account';
+import NotFound from './components/NotFound';
+import Login from './components/Auth/Login';
+import Signup from './components/Auth/Signup';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    checkAuthenticationStatus();
+  }, []);
+
+  const checkAuthenticationStatus = async () => {
+    // Perform the authentication status check here
+    // Example:
+    const token = localStorage.getItem('token');
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token);
+        const currentTime = Date.now() / 1000;
+        if (decodedToken.exp > currentTime) {
+          setIsAuthenticated(true);
+        } else {
+          setIsAuthenticated(false);
+        }
+      } catch (error) {
+        console.error('Error decoding JWT token:', error);
+        setIsAuthenticated(false);
+      }
+    } else {
+      setIsAuthenticated(false);
+    }
+    setIsLoading(false);
+  };
+
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Layout/>}>
-            <Route index element={<Home/>} />
-            <Route path="/products" element={<Products/>} />
-            <Route path="/products/:id" element={<Product/>} />
-            <Route path="/cart" element={<Cart/>} />
-            <Route path="/favorites" element={<Favorites/>} />
-            <Route path="/account" element={<Account/>} />
-            <Route component={<NotFound/>} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<Product />} />
+            {isAuthenticated ? (
+              <>
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/favorites" element={<Favorites />} />
+                <Route path="/account" element={<Account />} />
+              </>
+            ) : (
+              <Route element={<Navigate to="/login" />} />
+            )}
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </Router>
---
+++ src/components/Auth/Login.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="max-w-md w-full mx-auto p-8 bg-white rounded-md shadow-md">
+        <h2 className="text-3xl font-semibold text-gray-800 mb-6">Login</h2>
+        <form>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="border border-gray-400 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+              id="email"
+              type="email"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="border border-gray-400 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+            />
+          </div>
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
