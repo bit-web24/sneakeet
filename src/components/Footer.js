6e517fdec072b8a6fe05b4d51871a63c590c902a
Rishabh Raj Kumar <108469021+Rishabh-raj-kumar@users.noreply.github.com>
2023-06-19 08:26:48 +0530

    something modified (#3)
    * changed succes
    
    * changes again

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800">
+    <footer className=" bg-gray-800">
       <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center">
           <div className="text-gray-400">
