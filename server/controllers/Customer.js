14a339eb3ad8ad1429656ba5726d5acfd4c6debe
bit-web24 <bitweb24@gmail.com>
2023-08-04 14:12:43 +0530

    rm cart

--- server/controllers/Customer.js
+++ server/controllers/Customer.js
@@ -1,10 +1,8 @@
-const Cart = require('./customer/Cart');
 const Orders = require('./customer/Orders');
 const Favorites = require('./customer/Favorites');
 const Profile  = require('./customer/Profile');
 
 const Customer = {
-    cart: Cart,
     favorites: Favorites,
     orders: Orders,
     profile: Profile
