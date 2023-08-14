a656481f7b8d67432587dedeeb2a2061852598c2
Bittu Kumar <bitweb24@gmail.com>
2023-08-14 14:04:10 +0530

    Update serve.js

--- server/serve.js
+++ server/serve.js
@@ -39,8 +39,8 @@ app.use('/api', router);
 app.use('/admin', admin);
 
 // Start the server
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
 
-module.exports = server;
+module.exports = app;
